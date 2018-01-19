905647829d2aa5d5bb71f83f85b18a968bd149e1
Jim Shiba <james_shiba@datahouse.com>
2018-01-19 13:49:23 -0800

    End of [15.155] - test Firebase database created and environment file used.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBCaQZ1udNaNI56BV9be8xY19nkTGXjRLI",
-  authDomain: "expensify-4a37b.firebaseapp.com",
-  databaseURL: "https://expensify-4a37b.firebaseio.com",
-  projectId: "expensify-4a37b",
-  storageBucket: "expensify-4a37b.appspot.com",
-  messagingSenderId: "712665473234"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
